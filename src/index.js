7aa6533b8c377b9b17827eb7ec98c5d440430ce9
Yuliya Tsaruk <Juliamylife@yandex.ru>
2020-10-03 21:30:04 +0300

    feat: add new functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array !== undefined && array !== null && array.length > 0){
+    array.sort ((a,b) => a - b);
+    let minValue = array[0];
+    return minValue;
+  }else{
+    return 0;
+  }
+};
 
 exports.max = function max (array) {
-  return 0;
+  if (array !== undefined && array !== null && array.length > 0){
+    array.sort ((a,b) => b - a);
+    let maxValue = array[0];
+    return maxValue;
+  }else{
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (array !== undefined && array !== null && array.length > 0){
+  let sum = array.reduce ((a, b) => a+b, 0);
+  return sum/array.length;
+  }else{
+    return 0;
+  }
+};
